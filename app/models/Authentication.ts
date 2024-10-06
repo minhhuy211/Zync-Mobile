22890c41efc7a808071c76998f0a1b53ddd8a954
Tran Ngoc Tan Dat <21130314@st.hcmuaf.edu.vn>
2024-10-06 14:38:38 +0700

    up code model

---
+++ app/models/Authentication.ts
@@ -0,0 +1,35 @@
+export interface AuthenticateResponse {
+    accessToken: string;
+    refreshToken: string;
+  }
+  
+  export interface AuthenticateRequest {
+    email: string;
+    password: string;
+    twoFactorCode: string;
+  }
+  
+  export interface LoginRequest {
+    email: string;
+    password: string;
+  }
+  
+  export interface ReAuthenticateRequest {
+    refreshToken: string;
+  }
+  
+  export interface RegisterRequest {
+    email: string;
+    password: string;
+  }
+  
+  export interface ResetPasswordRequest {
+    email: string;
+    password: string;
+    newPassword: string;
+  }
+  
+  export interface VerificationRequest {
+      email: string;
+      code: string;
+      }
